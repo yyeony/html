1dfc50ea1843463c9f32ecb3ef23cc26c404ae04
DESKTOP-ERJ15JV\601-24 <aaa@aaa.com>
2025-06-05 16:42:25 +0900

    팝업처리

--- index.js
+++ index.js
@@ -7,3 +7,10 @@
         menu:'menu',
         anchors:['1','2','3','4']
     });
+
+    $('.d1').colorbox({rel:'gal'})
+  $('.d1pop').colorbox({iframe:true, innerWidth:800, innerHeight:600});
+
+  $('.win').on('click', ()=>{
+    window.open('./popup.html', 'popup', 'width=600m height=400');
+  })
